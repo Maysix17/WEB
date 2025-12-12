3f9e0b8917399c311583008a8799e6c1ec5e1249
Maysix17 <mayertrujillo60@gmail.com>
2025-12-11 19:07:29 -0500

    Original

--- API_Nest_Agro_Tic/src/actividades/actividades.controller.ts
+++ API_Nest_Agro_Tic/src/actividades/actividades.controller.ts
@@ -194,6 +194,13 @@ export class ActividadesController {
           cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
         },
       }),
+      fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith("image/")) {
+          cb(null, true);
+        } else {
+          cb(new Error("Only image files are allowed!"), false);
+        }
+      },
     }),
   )
   async finalizar(
--- API_Nest_Agro_Tic/src/ropa/dto/update-ropa.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRopaDto } from './create-ropa.dto';
-
-export class UpdateRopaDto extends PartialType(CreateRopaDto) {}
